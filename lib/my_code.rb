b681351cc2c706619ce2ffe50281142d6216ef24
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 10:59:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,5 @@
 # Your Code Here
-def my_own_map
+def my_own_map(array)
 new_array = []
 end
 
@@ -7,7 +7,7 @@ end
 
 
 
-def my_own_reduce
+def my_own_reduce(array, starting_point = 0)
 new_array = 0 
 
 end
