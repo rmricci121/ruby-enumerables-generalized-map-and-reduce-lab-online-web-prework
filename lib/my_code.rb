667af97c36408a890ec09261e26a419ba6ea086d
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 11:53:11 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # Your Code Here
+def map(array)
+new_array = []
+counter = 0 
+while counter < array.length do 
+  new_array.push(yield(array[counter]))
+counter += 1 
+end
+new_array
+end
+
+
+
+
+
+
+
+def reduce(array, starting_point = nil )
+if starting_point
+  total = starting_point
+  i=0 
+else
+  total = array[0]
+  i = 1 
+end
+while i < array.length do
+total = yield(total,array[i])
+i += 1 
+end
+total
+end
